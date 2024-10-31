b7c82425d879b1a4c62dee8592dea27ef520757c
OscarAndresMantillaTorres <186082954+OscarAndresMantillaTorres@users.noreply.github.com>
2024-10-31 17:35:45 -0500

    Solucion del ejercicio Division

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-def isleapYear(year):
-    
-    if year >= 1582:
-        
-        if (yeis_leap_yearar % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-            return True
-        else:
-            return False
-    else:
-        
-        return year % 4 == 0
+
+dividend = int(input("Enter the dividend: "))
+divisor = int(input("Enter the divisor: "))
 
 
-year = int(input("Enter a year: "))
-if isleapYear(year):
-    print(f"The year {year} It's a leap year.")
+quotient = dividend // divisor
+remainder = dividend % divisor
+
+
+if remainder == 0:
+    print("The division is exact.")
 else:
-    print(f"The year {year} It is not a leap year.")
+    print("The division is not exact.")
+
+
+print("Quotient:", quotient)
+print("Remainder:", remainder)
