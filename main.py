b978c81243235118bf7d63ffd7d4ddccee655570
OscarAndresMantillaTorres <186082954+OscarAndresMantillaTorres@users.noreply.github.com>
2024-10-31 18:07:09 -0500

    Solucion del ejercicio Ordenamiento

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 
-word1 = input("Enter the first word: ")
-word2 = input("Enter the second word: ")
+quantity = int(input("How many numbers do you want to enter? "))
 
 
-length1 = len(word1)
-length2 = len(word2)
+numbers = []
+for i in range(quantity):
+    number = int(input(f"Enter number {i + 1}: "))
+    numbers.append(number)
 
 
-if length1 > length2:
-    difference = length1 - length2
-    print(f"The word '{word1}' has {difference} more letters than '{word2}'.")
-elif length2 > length1:
-    difference = length2 - length1
-    print(f"The word '{word2}' has {difference} more letters than '{word1}'.")
-else:
-    print("The two words have the same length.")
+sorted_numbers = sorted(numbers)
+print("Sorted numbers:", " ".join(map(str, sorted_numbers)))
